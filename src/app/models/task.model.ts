5eebb2cb6d34b009e68c216fd121db49093070c0
Kass Badani <username@users.noreply.github.com>
2025-02-13 23:27:57 -0400

    Refactor Task model to eliminated status property

--- src/app/models/task.model.ts
+++ src/app/models/task.model.ts
@@ -1,19 +1,20 @@
 import {Category} from './category.model';
-import {Status} from './status.model';
 
 export class Task {
-
   id: number;
   name:  string;
   description: string;
   category: Category;
-  status: Status;
 
-  constructor(id: number, name: string, description: string, category: Category, status: Status)  {
+
+
+  constructor(id: number, name: string,
+              description :string ,
+              category: Category) {
     this.id = id;
     this.name = name;
     this.description = description;
     this.category = category;
-    this.status = status;
   }
 }
+
