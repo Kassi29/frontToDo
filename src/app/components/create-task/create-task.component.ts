165f99300027d1d8a0dfec79c69785715ed9ba84
Kass Badani <username@users.noreply.github.com>
2025-02-14 14:39:09 -0400

    Add create-task component

---
+++ src/app/components/create-task/create-task.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
+
+@Component({
+  selector: 'app-create-task',
+  templateUrl: './create-task.component.html',
+  standalone: true,
+  styleUrl: './create-task.component.css'
+})
+export class CreateTaskComponent {
+
+}
