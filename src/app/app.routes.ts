46a008c8801b8768edd99fe5ed15cd5ba0146ee0
Kass Badani <username@users.noreply.github.com>
2025-02-14 11:04:26 -0400

    Add routes for navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {CreateTaskComponent} from './components/create-task/create-task.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'create-task', component: CreateTaskComponent},
+
+];
