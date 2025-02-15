b8540da39114373eeb7d390bf42f0007e636975d
Kass Badani <username@users.noreply.github.com>
2025-02-15 16:46:23 -0400

    Refactor some styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import {CreateTaskComponent} from './components/create-task/create-task.component';
 import {HomeComponent} from './components/home/home.component';
 import {CreateCategoryComponent} from './components/create-category/create-category.component';
+import {ListCategoryComponent} from './components/list-category/list-category.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'create-task', component: CreateTaskComponent},
   {path: 'create-category', component: CreateCategoryComponent},
+  {path: 'list-categories', component: ListCategoryComponent}
 
 ];
