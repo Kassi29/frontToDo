2fedc4720814a67db7ca1b068ed627764b98354b
Kass Badani <username@users.noreply.github.com>
2025-02-15 12:40:38 -0400

    Add route /create-category

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import {CreateTaskComponent} from './components/create-task/create-task.component';
-import {AppComponent} from './app.component';
 import {HomeComponent} from './components/home/home.component';
+import {CreateCategoryComponent} from './components/create-category/create-category.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'create-task', component: CreateTaskComponent},
+  {path: 'create-category', component: CreateCategoryComponent},
 
 ];
